ca4a5f1c7fd883804ceee95ba6669fd67ead92f6
Kyle Knutson <kylerknutson@gmail.com>
2012-09-01 11:40:23 -0700

    Cleaned up the javascript a lot. Made better use of html classes, and moved all registering of listeners to the initalize method of the javascript file.

--- script.js
+++ script.js
@@ -1,22 +1,41 @@
-function navItemClicked(link, content) {
-	var linkElem = document.getElementById(link);
-	var contentElem = document.getElementById(content);
-	clearLiBackgrounds();
-	linkElem.className='selectedNavItem';
-	hideAllContent();
-	contentElem.style.display = 'inherit';
+// To initialize, we take every nav item and set up a click handler.
+function initialize() {
+  var navItems = document.querySelectorAll('.navItem');
+  for (var i = 0; i < navItems.length; i++) {
+    navItems.item(i).addEventListener('click', onClicked);
+  }
+  resetNavItems();
+  hideAllContent();
+  document.getElementById('homeNavItem').className = 'navItem selectedNavItem';
+  document.getElementById('homeContent').style.display = 'inherit';
 }
 
-function clearLiBackgrounds() {
-	document.getElementById('homeNavItem').className = '';
-	document.getElementById('blogNavItem').className = '';
-	document.getElementById('projectNavItem').className = '';
-	document.getElementById('videoNavItem').className = '';
+// Handles when a navItem is clicked. When one is clicked, we
+// reset the class names of the other navItems, mark our as set,
+// hide all display divs, and show the one ours is tied to.
+function onClicked() {
+  resetNavItems();
+  this.className = 'navItem selectedNavItem';
+  var id = this.id;
+  var prefix = id.substring(0, id.lastIndexOf('NavItem'));
+  var contentId = prefix + "Content";
+  hideAllContent();
+  document.getElementById(contentId).style.display = 'inherit';
+}
+
+function navItemClicked(link, content) {
+}
+
+function resetNavItems() {
+  var navItems = document.querySelectorAll('.navItem');
+  for (var i = 0; i < navItems.length; i++) {
+    navItems.item(i).className = 'navItem';
+  }
 }
 
 function hideAllContent() {
-	document.getElementById('homeContent').style.display = 'none';
-	document.getElementById('blogContent').style.display = 'none';
-	document.getElementById('projectContent').style.display = 'none';
-	document.getElementById('videoContent').style.display = 'none';
+	var contentItems = document.querySelectorAll('.content');
+  for (var i = 0; i < contentItems.length; i++) {
+    contentItems.item(i).style.display = 'none';
+  }
 }
